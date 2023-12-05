ab9b70e1b1b48252071bb3e9a29a73e7face12f6
Ashritagogula <147791711+Ashritagogula@users.noreply.github.com>
2023-12-05 13:46:17 +0530

    #include <stdio.h>
    int main() {
        int number, firstdigit, lastdigit, sum;
        scanf("%d", &number);
        lastdigit = number % 10;
        while (number >=10)
        {
            number /= 10;
        }
        firstdigit = number;
        sum = firstdigit + lastdigit;
        printf("%d
    ", sum);
    }

---
+++ Sum_of__First_and_Last_digits_.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int number, firstdigit, lastdigit, sum;
+    scanf("%d", &number);
+    lastdigit = number % 10;
+    while (number >=10)
+    {
+        number /= 10;
+    }
+    firstdigit = number;
+    sum = firstdigit + lastdigit;
+    printf("%d
+", sum);
+}
