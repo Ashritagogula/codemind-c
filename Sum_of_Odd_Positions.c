6c5fda566e33ff1eea58ca999700a8248306f702
Ashritagogula <147791711+Ashritagogula@users.noreply.github.com>
2023-12-17 11:46:25 +0530

    #include <stdio.h> int main() {     int arr[100];     int n;     scanf("%d", &n);     for (int i = 0; i < n; i++) {         scanf("%d", &arr[i]);     }     int sum = 0;     for (int i = 1; i < n; i +=2) {         sum += arr[i];     }     printf("%d ", sum);
    }

---
+++ Sum_of_Odd_Positions.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int arr[100]; 
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int sum = 0;
+    for (int i = 1; i < n; i +=2) {
+        sum += arr[i];
+    }
+    printf("%d
+", sum);
+
+}
