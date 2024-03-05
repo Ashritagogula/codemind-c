b6cbf03145d9da596909bb403889a6729163a7a3
Ashritagogula <147791711+Ashritagogula@users.noreply.github.com>
2024-03-05 18:45:52 +0530

    #include <stdio.h>
    // Function to check if it's possible to distribute cookies as required
    int can_distribute_cookies(int N, int X) {
        // Check if it's possible to distribute cookies as required
        if (N < X * (X + 1) / 2) {
            return 0;  // Not enough cookies for each student to have a distinct number
        }
    
        return 1;  // Distribution is possible
    }
    
    int main() {
        // Input values for N and X (number of cookies and students)
        int N, X;
        scanf("%d", &N);
        scanf("%d", &X);
    
        // Check if distribution is possible
        int result = can_distribute_cookies(N, X);
        if (result) {
            printf("YES
    ");
        } else {
            printf("NO
    ");
        }
    
        return 0;
    }

--- Sharing_is_Caring.c
+++ Sharing_is_Caring.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
+
+// Function to check if it's possible to distribute cookies as required
 int can_distribute_cookies(int N, int X) {
+    // Check if it's possible to distribute cookies as required
     if (N < X * (X + 1) / 2) {
-        return 0;
+        return 0;  // Not enough cookies for each student to have a distinct number
     }
 
-    return 1;  // Distribution
+    return 1;  // Distribution is possible
 }
 
 int main() {
+    // Input values for N and X (number of cookies and students)
     int N, X;
     scanf("%d", &N);
     scanf("%d", &X);
+
+    // Check if distribution is possible
     int result = can_distribute_cookies(N, X);
     if (result) {
         printf("YES
