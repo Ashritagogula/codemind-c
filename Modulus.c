71bc2c8add70ac06fb2f6e3f9bb1f5c1edf085f1
Ashritagogula <147791711+Ashritagogula@users.noreply.github.com>
2024-03-13 14:11:06 +0530

    #include<iostream> using namespace std; int main() {     int num1;     int num2;     cin>> num1;     cin>> num2;     int mod= num1%num2;     cout << mod; }

--- Modulus.c
+++ Modulus.c
@@ -1,7 +1,11 @@
-#include<stdio.h>
+#include<iostream>
+using namespace std;
 int main()
 {
-    int a,b;
-    scanf("%d%d",&a,&b);
-    printf("%d",a%b);
+    int num1;
+    int num2;
+    cin>> num1;
+    cin>> num2;
+    int mod= num1%num2;
+    cout << mod;
 }
